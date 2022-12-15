9ae97c4d5c1d50cb37bf90fe93a1b56fd9b39d5e
ecosolgit <119284598+ecosolgit@users.noreply.github.com>
2022-12-15 10:05:48 +0100

    Improvement when calling msi

--- dep.ps1
+++ dep.ps1
@@ -6,17 +6,31 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     }
 }
 
+$fusionUninstaller = "C:\Program Files\FusionInventory-Agent\Uninstall.exe"
+
 $software = "GLPI Agent"
 $installed = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where { $_.DisplayName -match $software }) -ne $null
 
 If(-Not $installed) {
-
-    Start-Process -NoNewWindow -FilePath "C:\Program Files\FusionInventory-Agent\Uninstall.exe" -ArgumentList "/S" -Wait
+    
+    if(Test-Path $fusionUninstaller -PathType Leaf){
+        Start-Process -NoNewWindow -FilePath $fusionUninstaller -ArgumentList "/S" -Wait
+    }
 
     Invoke-WebRequest -Uri "https://github.com/glpi-project/glpi-agent/releases/download/1.4/GLPI-Agent-1.4-x64.msi" -OutFile "$HOME\Downloads\glpi_agent.msi"
 
-    msiexec.exe /i "$HOME\Downloads\glpi_agent.msi" /quiet RUNNOW=1 SERVER=https://glpi.ecomedis.ch:4443/front/inventory.php,https://glpi.ecomedis.ch/front/inventory.php
+    Write-Host "Installing..."
 
-} else {
-	Write-Host "'$software' is installed."
+    $arguments = @(
+        "/i"
+        "$HOME\Downloads\glpi_agent.msi"
+        "/quiet"
+        "RUNNOW=1"
+        "SERVER=https://glpi.ecomedis.ch:4443/front/inventory.php,https://glpi.ecomedis.ch/front/inventory.php"
+    )
+    Start-Process C:\Windows\System32\msiexec.exe -ArgumentList $arguments -wait
 }
+
+Write-Host "$software is installed."
+
+pause
